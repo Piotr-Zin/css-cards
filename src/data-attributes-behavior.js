e7b973f1f53e6a827642c0e836135463f3af8cf7
Piotr Ziń <piotrzinbdg@gmail.com>
2018-07-24 20:44:34 +0200

    Removed unnecessary event listener.

--- src/data-attributes-behavior.js
+++ src/data-attributes-behavior.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', parse);
-
 export function parse() {
   var avatarDivs = document.querySelectorAll('.avatar');
 
-  avatarDivs.forEach((div, index, li) => {
+  avatarDivs.forEach((div, _index, _li) => {
     if (div.dataset.color) {
       div.style = `background-color: ${div.dataset.color}`;
     }
